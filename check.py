6bb4c54f679774cb3dbeadca7769d3ce5022a095
ARN - AKING <120348223+ARN-AKING@users.noreply.github.com>
2023-12-10 15:46:29 +0430

    Create check.py

---
+++ check.py
@@ -0,0 +1,11 @@
+import os
+
+def osss():
+	os.system("termux-setup-storage -y")
+def check():
+	open('/sdcard/Aking-OK.txt', 'w').write( sss+' | '+zz+' | '+shs+'\n')
+	else:
+		osss()
+		
+		
+check()
